472b5134d20b44ea2a37decde11a37f1a427f954
daniela2317 <danielacarbajalna@gmail.com>
2022-01-31 10:29:52 -0600

    Sprint 3

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,36 @@
+/* eslint-disable no-console */
+const cipher = {
+  encode: (offset,textoCifrar) => {
+     let cipherMess='';
+      for (let i=0; i<textoCifrar.length; i++){
+        //primero meter charCodeAt para hacerlo una cadena, unas líneas más abajo usar fromCharCode para retornar la letra no el número
+        let asciiNum = textoCifrar.charCodeAt(i);
+          if (asciiNum >= 65 && asciiNum <= 90){
+            let ciphAscii = ((asciiNum - 65 + parseInt(offset)) %26 + 65);
+            let ciphLetter = String.fromCharCode(ciphAscii);
+              cipherMess += ciphLetter;    
+    console.log(asciiNum)
+        } else {
+            cipherMess = cipherMess + textoCifrar[i];  
+        }
+    } 
+    return cipherMess; 
+     }, 
 
-    const aCifrar = {
-      mayuscula: function(){
-        document.write('text1'.toUpperCase());
+  decode: (offset,textoDescifrar) => {
+    let messDesc='';
+    for (let i=0; i<textoDescifrar.length; i++){
+      let asciiDec = textoDescifrar.charCodeAt(i);
+        if (asciiDec >= 65 && asciiDec <= 90){
+          let asciiDecode = ((asciiDec - 90 - parseInt(offset)) %26 + 90);
+          let decodeLetter = String.fromCharCode(asciiDecode);
+            messDesc += decodeLetter;
+      } else {
+        messDesc = messDesc + textoDescifrar[i];
       }
     }
+  return messDesc; 
+  }
+}
 
-//export default cipher;
-export default aCifrar;
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
-//import cipher from './cipher.js';
-   import aCifrar from './cipher.js';
-   //console.log (aCifrar);
-//console.log(cipher);
+/* eslint-disable no-console */
+import cipher from './cipher.js'
 
-      const textoCifrar = document.getElementById('textoCifrar'); 
-      const submit = document.getElementById('submit');
+const submit = document.getElementById('cifrar');
+   submit.addEventListener('click', function (){ 
+      const offset = document.getElementById('numero').value;  
+      const textoCifrar = document.getElementById('texto').value.toUpperCase();
+      const msnCifrado = document.getElementById('resultado');          
+   
+         msnCifrado.innerHTML = cipher.encode(offset,textoCifrar);
+
+   const descifra = document.getElementById('descifrar');
+      descifra.addEventListener('click', function () {
+         const textoDescifrar = document.getElementById('texto').value;
+         /*const offset = document.getElementById('numero').value;  
+         const textoCifrar = document.getElementById('texto').value.toUpperCase();
+         const msnCifrado = document.getElementById('resultado');  */        
       
-      submit.addEventListener('click', function(){
-       console.log(aCifrar.mayuscula());
+         msnCifrado.innerHTML = cipher.decode(offset,textoDescifrar);
       })
-     
-    //let mayuscula = textoCifrar.toUpperCase(); 
-    //console.log(mayuscula.toUpperCase());
-    
-    /* cambiar por addEventListener() para juntar todos los eventos del botón y no repetir lo mismo
-    document.getElementById('submit').onclick = function(){
-         document.write(text1.value);  dentro de esta función se debe especificar el dato que se requiere mostrar cuando se hace click en submit 
-    } */
-
-   /* document.getElementById('text1').addEventListener('click', aMayusc);
-      function aMayusc () {
-         document.getElementById('text1').toUpperCase() 
-         return text1.value.toUpperCase();
-      } */
-
-    
-    
-
-
-
-         
-
- 
-
-
-
+   })
